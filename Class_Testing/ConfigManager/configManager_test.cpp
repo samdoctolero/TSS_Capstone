6c15199a6191fb4b7f801a8311925ffc56125cbc
Sam Doctolero <scdoctol@ucalgary.ca>
2015-11-23 18:04:20 -0700

    no message

--- Class_Testing/ConfigManager/configManager_test.cpp
+++ Class_Testing/ConfigManager/configManager_test.cpp
@@ -9,7 +9,7 @@ int main()
 	ConfigManager config;
 
 	cout << "Ping ground:" << config.ping_ground << "\n";
-	cout << "Bus Nmber: " << config.bus.number << "\n";
+	cout << "Bus Nmber: " << config.bus[1].number << "\n";
 
 	return 0;
 }
