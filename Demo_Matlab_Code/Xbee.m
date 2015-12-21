6aac9685699f90d8a7b7de8d8f8efabe1840476d
Sam Doctolero <scdoctol@ucalgary.ca>
2015-12-21 15:04:39 -0700

    Failed lol

--- Demo_Matlab_Code/Xbee.m
+++ Demo_Matlab_Code/Xbee.m
@@ -15,10 +15,11 @@ classdef Xbee < handle
         
         function complete = initialize(this)
             this.Object = serial(strcat('COM',num2str(this.port)));
-            set(this.Object,'BaudRate',9600);
+            set(this.Object,'BaudRate',57600);
             set(this.Object,'DataBits',8);
             set(this.Object,'Parity','None');
             set(this.Object,'StopBits',1);
+            set(this.Object,'Terminator','CR/LF');
             try
                 fopen(this.Object);
                 complete = true;
@@ -30,6 +31,34 @@ classdef Xbee < handle
         function [str] = getEcho(this)
             str = fscanf(this.Object,'%s');
             %fprintf('echo: %s\n',echo);
+        end
+        
+        function error = setATmode(this)
+            try
+                %fwrite(this.Object,sprintf('%x','+++'));
+                fwrite(this.Object,strcat(sprintf('+++'),'0D'));
+                %this.Object.atCommand('+++');
+                error = false;
+            catch ex
+                error = true;
+                disp('Error on set AT');
+            end
+        end
+        
+        function rx = sendAndReceive(this,cmd)
+            if(~this.setATmode())
+                fwrite(this.Object,strcat(sprintf('%x',cmd),'0D'));
+                pause(1);
+                if(this.Object.BytesAvailable > 0)
+                    rx = fread(this.Object);
+                else
+                    rx = '';
+                    disp('No Return');
+                end
+            else
+                rx = '';
+            end
+
         end
     end
     
--- Demo_Matlab_Code/main.m
+++ Demo_Matlab_Code/main.m
@@ -1,8 +1,15 @@
-x = Xbee(4);
+delete(instrfindall);
+x = Xbee(5);
 if(~x.initialize())
  disp('Error. Cannot connect to Xbee');
 end
 
-disp(x.getEcho);
+if (x.Object.BytesAvailable ~= 0)
+    disp(x.getEcho);
+end
+
+
+    disp('Good');
+    disp(x.sendAndReceive('AT'));
 
 delete(x.Object);
